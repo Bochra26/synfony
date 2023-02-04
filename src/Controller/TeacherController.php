24fba2295c3eac02787ec64f328760eccf005c7e
Bochra26 <bochra.elgaabeb@esprit.tn>
2023-02-04 22:24:03 +0100

    First Commit

---
+++ src/Controller/TeacherController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+
+class TeacherController extends AbstractController
+{
+    #[Route('/teacher', name: 'app_teacher')]
+    public function index(): JsonResponse
+    {
+        return $this->json([
+            'message' => 'Welcome to your new controller!',
+            'path' => 'src/Controller/TeacherController.php',
+        ]);
+    }
+}
